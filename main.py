fe42b5a6b8d69c39403e3ad5f35c4d05bf5c0020
henrik392 <henrik.halvorsen.kvamme@gmail.com>
2020-09-04 21:57:38 +0200

    Path thingy

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-<<<<<<< HEAD
-=======
 from selenium import webdriver
+import os
+
 
 def main():
-    browser_path = "/usr/bin/brave-browser"
+    browser_path = os.environ["BROWSER_PATH"]
 
-    option = webdriver.ChromeOptions()
-    option.binary_location = browser_path
+    browser = ""
+    if browser_path == "":
+        browser = webdriver.Chrome()
+    else:
+        option = webdriver.ChromeOptions()
+        option.binary_location = browser_path
 
-    browser = webdriver.Chrome(options=option)
+        browser = webdriver.Chrome(options=option)
 
     browser.get("https://www.epicgames.com/store/en-US/free-games")
 
+
 if __name__ == "__main__":
     main()
->>>>>>> refs/remotes/origin/master
