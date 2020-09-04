985f0330225a7247e8109230a00628eebaa3d06a
danOne23 <danonemun@gmail.com>
2020-09-04 21:38:37 +0200

    Loads sites using brave

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import selenium
+from selenium import webdriver
 
 def main():
+    browser_path = "/usr/bin/brave-browser"
 
+    option = webdriver.ChromeOptions()
+    option.binary_location = browser_path
+
+    browser = webdriver.Chrome(options=option)
+
+    browser.get("https://www.epicgames.com/store/en-US/free-games")
 
 if __name__ == "__main__":
     main()
