be14d2ad21c696446c622816d67f460e7c3da8ba
henrik392 <henrik.halvorsen.kvamme@gmail.com>
2020-09-07 11:24:54 +0200

    Leads to login page.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+
 import os
+from time import sleep
+
+
+def clickOn(browser, xPath):
+    while True:
+        try:
+            button = browser.find_element_by_xpath(xPath)
+            button.click()
+            break
+        except:
+            sleep(1)
 
 
 def main():
+    browser = ""
     try:
         browser_path = os.environ["BROWSER_PATH"]
         option = webdriver.ChromeOptions()
@@ -13,6 +26,21 @@ def main():
         browser = webdriver.Chrome()
 
     browser.get("https://www.epicgames.com/store/en-US/free-games")
+
+    freeGamePath = '//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[2]/div[2]/div/div/section/div/div[1]/div/div/a'
+    clickOn(browser, freeGamePath)
+    clickOn(
+        browser, '//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div[2]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[3]/div/button')
+    clickOn(browser, '//*[@id="login-with-epic"]')
+
+    # Auto login
+
+    # Make purchase
+
+    # clickOn(browser, '//*[@id="purchase-app"]/div/div[4]/div[1]/div[2]/div[4]/div/div/label/div[1]/span/i[1]')
+    # clickOn(browser, '//*[@id="purchase-app"]/div/div[4]/div[1]/div[2]/div[5]/div/div/button')
+
+    sleep(200)
 
 
 if __name__ == "__main__":
